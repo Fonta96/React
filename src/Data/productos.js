8cb0b7ff9fd8a16c20f78ca94308a914ee712add
ivan fontanillo <ivanfontanillo@gmail.com>
2022-10-30 16:57:42 -0300

    2da Preentrega

---
+++ src/Data/productos.js
@@ -0,0 +1,7 @@
+export const newProducts = [
+    { id: 1, description: "Blvgari", name: "Man in Black", stock: 10 },
+    { id: 2, description: "Paco Rabanne", name: "Invictus", stock: 8 },
+    { id: 3, description: "Dior", name: "Sauvage", stock: 4 },
+    { id: 4, description: "Creed", name: "Aventus", stock: 15 },
+    { id: 5, description: "Carolina Herrera", name: "BadBoy", stock: 21 }
+];
--- src/components/containers/ItemListContainer/index.jsx
+++ src/components/containers/ItemListContainer/index.jsx
@@ -1,12 +1,44 @@
 import React from "react"
+import { useEffect } from "react"
 import "./style.css"
+import { newProducts } from "../../../Data/productos"
+import { useState } from "react"
 
-export default function ItemListContainer ({greeting}) {
-    return(
-        <div className="item-list-container">
-        <h2>{greeting}</h2>
-        </div>
+export default function ItemListContainer({ greeting }) {
+
+
+    const [products, setProducts] = useState([])
+    useEffect(() => {
+
+        (async () => {
+            const obtenerProductos = () => {
+                return new Promise((resolve, reject) => {
+                    setTimeout(() => {
+                        resolve(newProducts);
+                    }, 2000)
+                });
+            }
+
+            const response = await obtenerProductos()
+            console.log(response)
+            setProducts(response)
+        })()
+
+    }, [])
+
+    return (
+
+        <>
+            {products.map(products => {
+                return <p key={products.id}>{products.description}, stock:{products.stock}</p>
+            })}
+            <div className="item-list-container">
+                <h2>{greeting}</h2>
+            </div>
+            < itemList products={products} />
+        </>
     )
+
 }
 
 
